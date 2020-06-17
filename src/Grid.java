b06b5ec98643241349a368c8877ebb76c751b746
gavindang2911 <hoaidang2999@gmail.com>
2020-06-17 14:40:16 -0700

    create maze

--- src/Grid.java
+++ src/Grid.java
@@ -2,6 +2,7 @@ public class Grid {
     public static void main(String[] args) {
         MazeGenerator mazeBoard = new MazeGenerator(15, 20);
 
+        mazeBoard.createMaze();
         mazeBoard.displayMaze();
         mazeBoard.displayMazeString();
     }

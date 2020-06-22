f9d50f7a36323b95efe19df9e67e3ca49842fb3c
Peter <duyphuocnk92@gmail.com>
2020-06-22 00:22:05 -0700

    update logic

--- src/Grid.java
+++
@@ -1,9 +0,0 @@
-public class Grid {
-    public static void main(String[] args) {
-        MazeGenerator mazeBoard = new MazeGenerator(15, 20);
-
-        mazeBoard.createMaze();
-        mazeBoard.displayMaze();
-        mazeBoard.displayMazeString();
-    }
-}
